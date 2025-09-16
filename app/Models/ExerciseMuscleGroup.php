f6afcc226a96093544580152bd5f73cc1d7ac50c
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 21:15:51 +0800

    Added primary key, timestamps, fillable attributes, casts, relationships for exercise muscle group model file

--- app/Models/ExerciseMuscleGroup.php
+++ app/Models/ExerciseMuscleGroup.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ExerciseMuscleGroup extends Model
 {
-    //
+    protected $primaryKey = 'exercise_muscle_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'exercise_id',
+        'muscle_group_id',
+        'primary_target',
+        'activation_percentage',
+    ];
+
+    protected $casts = [
+        'primary_target' => 'boolean',
+        'activation_percentage' => 'decimal:2',
+    ];
+
+    public function exercise(): BelongsTo
+    {
+        return $this->belongsTo(Exercise::class, 'exercise_id', 'exercise_id');
+    }
+
+    public function muscleGroup(): BelongsTo
+    {
+        return $this->belongsTo(MuscleGroup::class, 'muscle_group_id', 'muscle_group_id');
+    }
 }
