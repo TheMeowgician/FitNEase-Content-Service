c03bfc56c30a73dfdbf5d4c9da8c8499f09f9539
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 21:17:09 +0800

    Added primary key, fillable attributes, casts, relationship for exercise instruction model file

--- app/Models/ExerciseInstruction.php
+++ app/Models/ExerciseInstruction.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ExerciseInstruction extends Model
 {
-    //
+    protected $primaryKey = 'instruction_id';
+
+    protected $fillable = [
+        'exercise_id',
+        'instruction_type',
+        'instruction_text',
+        'step_order',
+        'is_critical',
+    ];
+
+    protected $casts = [
+        'step_order' => 'integer',
+        'is_critical' => 'boolean',
+    ];
+
+    public function exercise(): BelongsTo
+    {
+        return $this->belongsTo(Exercise::class, 'exercise_id', 'exercise_id');
+    }
 }
