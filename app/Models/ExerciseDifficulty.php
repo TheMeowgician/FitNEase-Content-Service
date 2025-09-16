a2ef2e82c427b17ab574e34eb514f93acbf0e8fd
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 21:10:59 +0800

    Added primary key, fillable attributes, casts for exercise difficulty model file

--- app/Models/ExerciseDifficulty.php
+++ app/Models/ExerciseDifficulty.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExerciseDifficulty extends Model
 {
-    //
+    protected $primaryKey = 'difficulty_id';
+
+    protected $fillable = [
+        'difficulty_name',
+        'description',
+        'min_experience_months',
+        'recommended_fitness_level',
+        'intensity_scale',
+    ];
+
+    protected $casts = [
+        'min_experience_months' => 'integer',
+        'intensity_scale' => 'decimal:2',
+    ];
 }
