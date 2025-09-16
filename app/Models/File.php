5b2ec59e78fa6e56b10b242703fff8323efc0916
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 21:15:01 +0800

    Added primary key, timestamps, fillable attributes, casts, relationships for file model

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $primaryKey = 'file_id';
+    public $timestamps = false;
+
+    protected $dates = ['uploaded_at'];
+
+    protected $fillable = [
+        'file_name',
+        'original_file_name',
+        'file_path',
+        'file_type',
+        'file_size_bytes',
+        'mime_type',
+        'uploaded_by',
+        'entity_type',
+        'entity_id',
+        'is_public',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'file_size_bytes' => 'integer',
+        'entity_id' => 'integer',
+        'is_public' => 'boolean',
+        'is_active' => 'boolean',
+        'uploaded_at' => 'datetime',
+    ];
 }
