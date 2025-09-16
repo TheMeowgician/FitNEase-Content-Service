bdc24752e4940bc569bcc89961d820d4dce38e58
TheMeowgician <gabsmaglaya@gmail.com>
2025-09-16 21:11:46 +0800

    Added primary key, timestamps, fillable attributes, casts, relationships for muscle group model file

--- app/Models/MuscleGroup.php
+++ app/Models/MuscleGroup.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class MuscleGroup extends Model
 {
-    //
+    protected $primaryKey = 'muscle_group_id';
+
+    protected $fillable = [
+        'group_name',
+        'description',
+        'primary_muscles',
+        'secondary_muscles',
+        'exercise_benefits',
+    ];
+
+    public function exercises(): BelongsToMany
+    {
+        return $this->belongsToMany(Exercise::class, 'exercise_muscle_groups', 'muscle_group_id', 'exercise_id')
+            ->withPivot(['primary_target', 'activation_percentage']);
+    }
 }
